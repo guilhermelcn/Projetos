bda9d02badc13f58d03af5f3fd3b38ce21c236ee
GuilhermeLucio87 <guilcn2@hotmail.com>
2017-09-09 16:23:08 -0300

    Marrydog_angular

---
+++ Marrydog_Angular_Repeat/public/js/controllers/fotos-controller.js
@@ -0,0 +1,23 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+
+	$scope.fotos= [{
+
+    titulo : 'Bassset',
+		url : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY5Ll6ASApaYmZHan0xbR8uFmQm-9Qawdq6ZsH592AAtZuzf4b'
+  },
+
+    {
+
+    titulo : 'Golden',
+    url : 'http://s7d2.scene7.com/is/image/PetSmart/PB1201_STORY_CARO-Authority-HealthyOutside-DOG-20160818?$PB1201$'
+    },
+
+    {
+
+    titulo : 'Husky',
+    url : 'https://http2.mlstatic.com/estou-doando-3-filhotes-de-husky-siberiano-liga-64-9321-7970-D_NQ_NP_581111-MLB20486151877_112015-F.jpg'
+
+    }
+  ];
+
+});
